56ad43b22951d70e8bf844f23753d9bde0cac64e
robertheo <robertheo.rt@gmail.com>
2022-01-27 04:59:40 +1400

    update

--- controllers/admin/routes.py
+++ controllers/admin/routes.py
@@ -2,18 +2,20 @@ from flask import Flask, render_template, session, redirect
 from app import app
 from controllers.admin.models import Admin
 from controllers.attendance.models import Attendance
+from datetime import datetime, timedelta, date
 
 @app.route("/admin/user/")
 def adminUser():
     users = Attendance().getReport()
-    from datetime import datetime, timedelta, date
+    
     today = datetime.today().strftime("%m/%d/%Y")
-    return render_template('admin/user.html', myUsers=users,today=today)
+    return render_template('admin/user.html', myUsers=users, today = today)
    
 @app.route("/admin/absensi/")
 def absensi():
     reports = Attendance().getReport()
-    return render_template('admin/absensi.html', myReports = reports)
+    today = datetime.today().strftime("%m/%d/%Y")
+    return render_template('admin/absensi.html', myReports = reports, today = today)
 
 @app.route("/admin/report/")
 def report():
